46c3e817093ee08e114a9f70939a72522b11d671
Brosseau <xavierb.2006@hotmail.com>
2015-12-07 15:56:09 -0500

    ajout d'un module de création de compte (marche pas encore)

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 echo("
 <div style='text-align: center;width: auto'>
     <br/>
+    <h3>Connexion</h3>
     <form action='index.php' method='post'>
         Identifiant:<br>
         <input type='text' name='username'>
@@ -10,6 +11,23 @@ echo("
         <input type='password' name='psw'>
         <br><br>
         <input type='submit' value='Connexion'>
+    </form>
+    <hr>
+    <h3>Pas de compte ? Crée toi en un !</h3>
+    <form action='index.php' method='post'>
+        Nom complet:<br>
+        <input type='text' name='fullname_n'>
+        <br>
+        Identifiant:<br>
+        <input type='text' name='username_n'>
+        <br>
+        Mot de passe:<br>
+        <input type='password' name='psw_n1'>
+        <br>
+        Confirmer mot de passe:<br>
+        <input type='password' name='psw_n2'>
+        <br><br>
+        <input type='submit' value='Créer'>
     </form>
 </div>
 
